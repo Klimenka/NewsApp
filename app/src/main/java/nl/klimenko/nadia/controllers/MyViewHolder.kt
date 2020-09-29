8178803494ba82c5dafb1461551c281ec78a8c6c
Nadia Klimenko <nmk2502@gmail.com>
2020-09-29 14:34:21 +0200

    the titles showed properly

--- app/src/main/java/nl/klimenko/nadia/controllers/MyViewHolder.kt
+++ app/src/main/java/nl/klimenko/nadia/controllers/MyViewHolder.kt
@@ -3,14 +3,22 @@ package nl.klimenko.nadia.controllers
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import coil.transform.CircleCropTransformation
+import coil.transform.RoundedCornersTransformation
 import kotlinx.android.synthetic.main.articles_view.view.*
+import nl.klimenko.nadia.R
 import nl.klimenko.nadia.models.Article
 
 class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind (item: Article) {
         itemView.Title.text = item.Title
-        itemView.Image.load(item.Image)
+        itemView.Image.load(item.Image){
+            crossfade(true)
+           // placeholder(R.drawable.placeholder)
+            transformations(RoundedCornersTransformation(10F))
+
+        }
 
     }
 }
