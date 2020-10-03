c8d0253f5fecf04add33d22ff25f1ce9700f1226
Nadia Klimenko <nmk2502@gmail.com>
2020-10-03 16:26:03 +0200

    clickable url

--- app/src/main/java/nl/klimenko/nadia/views/DetailedArticle.kt
+++ app/src/main/java/nl/klimenko/nadia/views/DetailedArticle.kt
@@ -3,14 +3,15 @@ package nl.klimenko.nadia.views
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
 import coil.load
 import nl.klimenko.nadia.R
 import nl.klimenko.nadia.models.Article
+import nl.klimenko.nadia.models.User
 
 class DetailedArticle : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
@@ -21,6 +22,15 @@ class DetailedArticle : AppCompatActivity() {
         findViewById<ImageView>(R.id.imageDetailed).load(item.Image)
         findViewById<TextView>(R.id.textDetailed).movementMethod = LinkMovementMethod.getInstance()
         findViewById<TextView>(R.id.textDetailed).text = HtmlCompat.fromHtml(item.Summary.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
+        findViewById<TextView>(R.id.linkToTheArticle).text =item.Url.toString()
+        if(User.getUser().token!=null){
+            if(!item.IsLiked!!){
+                findViewById<ImageView>(R.id.heart).visibility = View.VISIBLE
+            }
+            else{
+                findViewById<ImageView>(R.id.heartLike).visibility = View.VISIBLE
+            }
+        }
         Log.i("Detail", "Open article with title ${item.Title}")
     }
 }
