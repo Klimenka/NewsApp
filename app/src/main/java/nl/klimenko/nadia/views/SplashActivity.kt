9e7db1d6496fb2fbe509582bf04bb031fffd935b
Nadia Klimenko <nmk2502@gmail.com>
2020-10-04 21:14:31 +0200

    splash screen

---
+++ app/src/main/java/nl/klimenko/nadia/views/SplashActivity.kt
@@ -0,0 +1,24 @@
+package nl.klimenko.nadia.views
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import nl.klimenko.nadia.R
+
+class SplashActivity : AppCompatActivity() {
+
+    // This is the loading time of the splash screen
+    private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+        Looper.myLooper()?.let {
+            Handler(it).postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, SPLASH_TIME_OUT)
+        }
+    }
+}
